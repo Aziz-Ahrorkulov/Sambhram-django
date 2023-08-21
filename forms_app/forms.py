6f39da5bef69c3fd2ccf4275a9a3c13b220fa962
Aziz-Ahrorkulov <137227605+Aziz-Ahrorkulov@users.noreply.github.com>
2023-08-21 20:22:21 +0500

    Revert "Revert "end the filter and end apply and reject""
    This reverts commit c0f2888421f44f842b3e76de573179bd26fbaa41.

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,18 +1,19 @@
 from django import forms
 from .models import JobApplication, Gender
+from phonenumber_field.formfields import PhoneNumberField
 
 class JobApplicationForm(forms.ModelForm):
 
     first_name = forms.CharField(label='Full Name', widget=forms.TextInput(attrs={'id': 'firstName', 'class': 'form-control form-control-lg', 'type' : 'text'}))
     last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'id': 'lastName', 'class': 'form-control form-control-lg', 'type' : 'text'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'id':' emailAddress', 'class': 'form-control form-control-lg', 'type' : 'email'}))
-    phone = forms.IntegerField(label='Email', widget=forms.NumberInput(attrs={'id':' phoneNumber', 'class': 'form-control form-control-lg', 'type' : 'tel'}))
+    phone = PhoneNumberField(label='Phone', widget=forms.NumberInput(attrs={'id':' phoneNumber', 'class': 'form-control form-control-lg', 'type' : 'tel'}))
     resume = forms.FileField(label='File', widget=forms.FileInput(attrs={'type': 'file', 'name': 'resume', 'accept' : '.pdf,.doc,.docx', 'id' : 'resume'}))
-    birthday = forms.DateField(label='Date', widget=forms.DateInput(attrs={'id': 'birthdayDate', 'type': 'date', 'class': 'form-control form-control-lg',}))
+    birtday = forms.DateField(label='Date', widget=forms.DateInput(attrs={'id': 'birthdayDate', 'type': 'date', 'class': 'form-control form-control-lg',}))
     gender = forms.ChoiceField(choices=Gender.choices, widget=forms.RadioSelect(attrs={'class' : 'form-check-input', 'name' : 'inlineRadioOptions', 'type' : 'radio'}))
 
 
 
     class Meta:
         model = JobApplication
-        fields = ['first_name', 'last_name', 'email', 'phone', 'birthday', 'resume', 'gender']
+        fields = ['first_name', 'last_name', 'email', 'phone', 'birtday', 'resume', 'gender']
