c0f2888421f44f842b3e76de573179bd26fbaa41
Aziz-Ahrorkulov <137227605+Aziz-Ahrorkulov@users.noreply.github.com>
2023-08-21 20:20:06 +0500

    Revert "end the filter and end apply and reject"
    This reverts commit 487e41b48a9f9af1e76d65c1dc028f6674255249.

--- forms_app/models.py
+++ forms_app/models.py
@@ -3,9 +3,6 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
-
-
 
 class Gender(models.TextChoices):
     MALE = 'M', 'Male'
@@ -13,20 +10,13 @@ class Gender(models.TextChoices):
     OTHER = 'O', 'Other'
 
 class JobApplication(models.Model):
-    APPROVAL_STATUSES = (
-        (0, 'Ожидает одобрения'),
-        (1, 'Одобрено'),
-        (2, 'Отклонено'),
-    ) 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = PhoneNumberField()
+    phone = models.IntegerField()
     birtday = models.DateField(blank=False)
     resume = models.FileField(upload_to='resumes/')
     gender = models.CharField(max_length=1, choices=Gender.choices)
-    approval_status = models.CharField(max_length=20, choices=APPROVAL_STATUSES, default='pending')
-    current_admin_stage  = models.PositiveIntegerField(default=2)
 
     def __str__(self):
         return self.first_name
